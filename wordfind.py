e1be04e49765735edecfbd4e40f3c6f0e2ab5cd4
star0202 <61998307+star0202@users.noreply.github.com>
2021-07-09 08:59:32 +0900

    Append line system

--- wordfind.py
+++ wordfind.py
@@ -1,13 +1,16 @@
 class WordFindErrors(Exception):
     pass
-class WorngTableError(WordFindErrors):
+class WrongTableError(WordFindErrors):
     pass
-class Game():
-    def __init__(self, x, y, allowDiagonal, table, find):
+class Game:
+    def __init__(self, x:int, y:int, allowDiagonal:bool, table:list, find:list):
         self.x = x
         self.y = y
-        self.Digonal = allowDiagonal
+        self.digonal = allowDiagonal
         self.find = find
         self.table = table
+        self.lines ={}
         if len(self.table) % x != 0 or len(self.table) % y !=0:
             raise WorngTableError
+        for i in range(0, self.y):
+            self.lines[i+1] = self.table[i*x:(i+1)*x]
